b8bbca111d0a4f63dc9f397406607a775292feac
MadCindy <cindypereira93@gmail.com>
2022-02-01 15:06:27 +0100

    Update Karelian and Finnish

--- website/app.py
+++ website/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    
+
     return render_template('index.html', lang_1="", rules_lang_1="", lang_2="", rules_lang_2="")
-    
+
     #linguistic_analysis/extracted_rules
-    
+
     #English  Finnish  German  Karelian  Mezquital_otomi  Swedish
-    
+
 def route_print(language, dic, tabs=0):
-    
+
     text = ""
-    
+
     for key, val in dic.items():
         text += "\t" * tabs + '<t style="color:blue;">' + key + '</t>' #background-color:yellow;
         if type(val) == type(dict()):
@@ -26,18 +26,18 @@ def route_print(language, dic, tabs=0):
         if tabs == 0:
             text += "\n"
     return text
-    
+
 
 @app.route('/publish_rules',  methods=['POST'])
 def publish_rules():
     language1 = request.form.get('language1')
     language2 = request.form.get('language2')
-    
+
     dic_langs = {'english': 'english.json',
                  'german': 'german.json',
                  'swedish': 'swedish.json',
-                 'finnish': 'Finnish',
-                 'karelian': 'Karelian',
+                 'finnish': 'finnish.json',
+                 'karelian': 'karelian.json',
                  'otomi': 'otomi.json'}
     rel_path = "../linguistic_analysis/extracted_rules/"
     rules1 = ""
@@ -58,6 +58,5 @@ def publish_rules():
             rules2 = f.read()
     if not rules2:
         rules2 = route_print(language2, dic_2, tabs=0)
-    
+
     return render_template('index.html', lang_1 = language1.capitalize(), rules_lang_1=rules1, lang_2 = language2.capitalize(), rules_lang_2=rules2)
-    
